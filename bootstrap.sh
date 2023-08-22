c0f73c2989145dcdf7724af50fc0dfea547f3f76
Renan <renangarcia@live.com>
2023-08-22 11:44:01 -0300

    chore: cp oh-my-zsh folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Removes .zshrc from $HOME (if it exists) and copy the .zshrc file from the .dotfiles
+cp -r .oh-my-zsh $HOME/.oh-my-zsh
+
 rm -rf $HOME/.zshrc
 cp .zshrc $HOME/.zshrc
 
