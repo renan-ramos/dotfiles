3213ec7d89e86af55f48092c4437998111e0f4e5
Renan <renangarcia@live.com>
2023-08-22 11:42:04 -0300

    fix: bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 # Removes .zshrc from $HOME (if it exists) and copy the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
-cp -r .zshrc $HOME/.zshrc
+cp .zshrc $HOME/.zshrc
 
 source $HOME/.zshrc
